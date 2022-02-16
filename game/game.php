7af297ae9748b8b7659bc8b466ac79933ad53889
DanOG <66186709+danOGUK@users.noreply.github.com>
2022-02-16 12:11:16 +0000

    Update game.php

--- game/game.php
+++ game/game.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Game.io | Free Online Game | Play Now</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    
+    <button type="button" class="attackBtn" onClick="attackSpace()">Attack!</button>
+    <span id="spaceKills">0</span>
+    
+<script src="js/script.js"></script>
