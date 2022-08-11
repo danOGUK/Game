a50fd7ca537c86d1ca8c8dc976eb360ca2ea850d
DanOG <66186709+danOGUK@users.noreply.github.com>
2022-08-11 23:10:04 +0100

    Aluminium
    aluminium integration

--- game/js/script.js
+++ game/js/script.js
@@ -1,7 +1,16 @@
 var spaceKills = 0;
+var aluminium = 0;
 var bullets = 1;
+
+function randomAluminium() {
+let rand = Math.round(Math.random() * 4);
+aluminium = aluminium + rand;
+document.getElementById("alumQty").innerHTML = aluminium;
+}
 
 function attackSpace() {
  spaceKills = spaceKills + bullets;
    document.getElementById("spaceKills").innerHTML = spaceKills;
+  randomAluminium();
 }
+
