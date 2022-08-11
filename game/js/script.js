d938a56f6ab04ee6ad7c039e2394fd8d8a12ea39
DanOG <66186709+danOGUK@users.noreply.github.com>
2022-08-11 23:21:58 +0100

    Update script.js

--- game/js/script.js
+++ game/js/script.js
@@ -3,10 +3,9 @@ var aluminium = 0;
 var bullets = 1;
 
 function randomAluminium() {
-
 let rand = Math.floor(Math.random() * 10);
 aluminium = aluminium + rand;
-document.getElementById("alumQty").innerHTML = aluminium;
+document.getElementById("aluminium").innerHTML = aluminium;
 }
 
 function attackSpace() {
